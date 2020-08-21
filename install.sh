655639420942f02bb00d42e74bdcf07a76b919cb
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-21 17:00:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,37 @@
 #!/bin/sh
 
-echo "正在安装AdGuardHome DNS服务器. . ."
+clear
+
+echo "正在下载AdGuardHome DNS服务器. . ."
 
 wget https://static.adguard.com/adguardhome/beta/AdGuardHome_linux_amd64.tar.gz
+
+echo "正在安装AdGuardHome DNS服务器. . ."
 
 tar -zvxf AdGuardHome_linux_amd64.tar.gz 
 
 rm -rf AdGuardHome_linux_amd64.tar.gz 
 
-cd AdGuardHome && ./AdGuardHome -s install
+cd AdGuardHome && sudo ./AdGuardHome -s install
+
+echo "正在下载AdGuardHome配置文件. . ."
 
 wget https://github.com/HXHGTS/AdguardHomeInstall/raw/master/AdGuardHome.yaml -O /root/AdGuardHome/AdGuardHome.yaml
 
 systemctl restart AdGuardHome
 
-echo "DNS服务器已安装"
+clear
 
-echo "WebUI:http://<server_ip>"
+echo "--------------------------------------------"
 
-echo "用户名，密码均为root"
+echo "WebUI: http://<服务器ip地址>"
 
-echo "dns直接指向服务器ip即可！"
+echo "用户名: root"
 
+echo "密码: root"
+
+echo "DNS: <服务器ip地址>"
+
+echo "--------------------------------------------"
+
+echo "DNS服务器已安装，防火墙注意放行tcp53 udp53端口！"
