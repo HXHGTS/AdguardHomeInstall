0dc2931bbeee1aec78f70439e4736c1bc5f5663f
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-02-17 14:25:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 systemctl stop AdGuardhome && systemctl disable AdGuardhome
 
-rm -rf /root/AdGuardHome
+rm -rf /opt/AdGuardHome
 
 clear
 
 echo "正在下载AdGuardHome DNS服务器. . ."
 
-wget https://static.adguard.com/adguardhome/beta/AdGuardHome_linux_amd64.tar.gz -O /root/AdGuardHome_linux_amd64.tar.gz
+wget https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_amd64.tar.gz -O /opt/AdGuardHome_linux_amd64.tar.gz
 
 echo "正在安装AdGuardHome DNS服务器. . ."
 
@@ -16,11 +16,11 @@ tar -zvxf AdGuardHome_linux_amd64.tar.gz
 
 rm -rf AdGuardHome_linux_amd64.tar.gz
 
-cd /root/AdGuardHome && rm -rf LICENSE.txt && rm -rf README.md && ./AdGuardHome -s install
+cd /opt/AdGuardHome && rm -rf LICENSE.txt && rm -rf README.md && ./AdGuardHome -s install
 
 echo "正在下载AdGuardHome配置文件. . ."
 
-wget https://cdn.jsdelivr.net/gh/HXHGTS/AdguardHomeInstall/AdGuardHome.yaml -O /root/AdGuardHome/AdGuardHome.yaml
+wget https://cdn.jsdelivr.net/gh/HXHGTS/AdguardHomeInstall/AdGuardHome.yaml -O /opt/AdGuardHome/AdGuardHome.yaml
 
 systemctl restart AdGuardHome
 
