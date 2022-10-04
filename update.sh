5dca77a398f2eca6da99d08bd1e0c7a0971eddee
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-10-04 12:00:58 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-echo "正在停止AdGuardHome DNS服务器. . ."
+echo "正在停止并删除旧版本AdGuardHome DNS服务器. . ."
 
 systemctl stop AdGuardHome
 
 systemctl disable AdGuardHome
 
-mv -f /opt/AdGuardHome/AdGuardHome.yaml /opt/AdGuardHome.yaml
+rm -f /opt/AdGuardHome/AdGuardHome
 
-rm -rf /opt/AdGuardHome/
-
-echo "正在下载AdGuardHome DNS服务器. . ."
+echo "正在下载最新版本AdGuardHome DNS服务器. . ."
 
 wget https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_amd64.tar.gz -O /opt/AdGuardHome_linux_amd64.tar.gz
 
@@ -19,8 +17,6 @@ echo "正在安装AdGuardHome DNS服务器. . ."
 cd /opt && tar -zvxf AdGuardHome_linux_amd64.tar.gz
 
 rm -f /opt/AdGuardHome_linux_amd64.tar.gz
-
-mv -f /opt/AdGuardHome.yaml /opt/AdGuardHome/AdGuardHome.yaml
 
 systemctl start AdGuardHome
 
